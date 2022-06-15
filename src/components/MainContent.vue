1afb27ec74ad0d83ff520c779bb4475d0e0da35e
Bob-117 <bricaud.olivier.dev@gmail.com>
2022-06-16 01:56:23 +0200

    vue mainContent

---
+++ src/components/MainContent.vue
@@ -0,0 +1,33 @@
+<template>
+
+<NavBar/>
+  <router-view/>
+
+</template>
+
+<script>
+
+import NavBar from "@/components/NavBar";
+
+export default {
+  name: 'MainMainContent',
+  components: {
+    NavBar
+  },
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

d12bb63fca1fc9075c51dba7e05332a142dd61b1
Bob-117 <bricaud.olivier.dev@gmail.com>
2022-06-16 01:54:38 +0200

    vue main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import router from './router/router'
+
+// App.use(BootstrapVue)
+// App.use(IconsPlugin)
+createApp(App)
+    .use(router)
+    .mount('#app')
